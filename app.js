bcbf8a6e1ac4486048e6b7196ac28300b3aca6b8
Harshit Arora <harora1_be23@thapar.edu>
2024-02-09 17:15:04 +0530

    server

--- app.js
+++ app.js
@@ -1,16 +1,45 @@
 import express from "express";
+import bodyParser from "body-parser";
+import {dirname} from "path";
+import { fileURLToPath } from "url";
+import morgan from "morgan";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 
 
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(morgan("tiny"));
 
+app.post("/submit", (req,res) => {
+    console.log(req.body);
+});
 
 
 
 app.get("/", (req,res) => {
-    res.send("hello");
+    res.render(__dirname + "/views/login.ejs");
 });
 
+app.get("/about", (req,res) => {
+
+});
+
+app.get("/login", (req,res) => {
+
+});
+
+app.get("/user", (req,res) => {
+
+});
+
+app.post("/submit", (req,res) => {
+    res.sendStatus(200);
+});
+
+app.get("/vendor", (req,res) => {
+
+});
 
 
 
